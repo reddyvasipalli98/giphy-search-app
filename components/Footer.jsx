9babe8ab00521f8022aa9f5da5e46fefa91023f4
Karthikeya Reddy Vasipalli <reddyvasipalli98@outlook.com>
2025-07-29 16:45:44 -0400

    Update Footer with sample change

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer () {
             <div className="footer">
                 <p><Link href="/">home</Link></p>
                 <p><Link href="/about">about</Link></p>
-                <p>A big thanks to <a href="https://giphy.com/">giphy.com</a> for letting us use there API!</p>
+                <p>A big thanks to <a href="https://giphy.com/">giphy.com</a> for letting us use there API! It's just great!</p>
             </div>
         </>
     )
